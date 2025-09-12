85533012f75fc3563633a151f3df1b7927e3cd65
yeqiang02 <yeqiang02@corp.netease.com>
2025-09-12 10:38:32 +0800

    OMSEC-144156 【后端】【SaaS】点播音视频数据查询增加「ai生成」结果筛选/展示&java/go sdk支持
    【后端】【SaaS】点播音视频数据查询增加「ai生成」结果筛选/展示&java/go sdk支持
    
    (cherry picked from commit 7b05d6fc19d0d627ac1124edd6b367995ab029ab)

--- yidun/service/antispam/video/callback/v4/response/video_callback_aigc_response.go
+++ yidun/service/antispam/video/callback/v4/response/video_callback_aigc_response.go
@@ -19,6 +19,12 @@ type VideoDataAigcV4Response struct {
 type VideoDataAigcDetailBo struct {
 	// 是否ai生成
 	IsAigc    *bool                     `json:"isAigc,omitempty"`
+	//识别分数
+	AigcRate  *float64                  `json:"aigcRate,omitempty"`
+	//置信等级
+	AigcLevel  *int                     `json:"aigcLevel,omitempty"`
+	//标识信息
+	Signage  *AigcV5SignageResp         `json:"signage,omitempty"`
 }
 
 type VideoImageAigcV4Response struct {
@@ -31,5 +37,33 @@ type VideoImageAigcV4Response struct {
 type VideoImageAigcDetailBo struct {
 	// 是否ai生成
 	IsAigc    *bool                     `json:"isAigc,omitempty"`
+	//识别分数
 	AigcRate  *float64                  `json:"aigcRate,omitempty"`
+	//置信等级
+	AigcLevel  *int                     `json:"aigcLevel,omitempty"`
+	//标识信息
+	Signage  *AigcV5SignageResp         `json:"signage,omitempty"`
+}
+
+type AigcV5SignageResp struct {
+	// 显式标识
+	OvertSignage *int `json:"overtSignage,omitempty"`
+	// 隐式标识
+	CovertSignage *int `json:"covertSignage,omitempty"`
+	// 隐式标识详细信息
+	CovertSignageDetails *CovertSignageDetailsResp `json:"covertSignageDetails,omitempty"`
+}
+
+type CovertSignageDetailsResp struct {
+	// 角色
+	Role *int `json:"role,omitempty"`
+	// 平台信息
+	Platform *CovertSignageDetailsPlatformResp `json:"platform,omitempty"`
+}
+
+type CovertSignageDetailsPlatformResp struct {
+	// 1: 公司 2: 个人
+	Type *int `json:"type,omitempty"`
+	// 隐式生成的平台/个人信息
+	Info *string `json:"info,omitempty"`
 }
