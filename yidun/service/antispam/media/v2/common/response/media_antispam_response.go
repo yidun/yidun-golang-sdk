2cc5a4b136b507ccc57f5e4e1ed8312cc1ffe7c9
谢铁藜 <xietieli@corp.netease.com>
2025-11-13 16:42:46 +0800

    OMSEC-147298 融媒体SDK字段缺失补全 1、antispam下增加suggestionLevel、riskDescription字段
    (cherry picked from commit 6df5cff08f5f644533ce482e77ed446f1c50c3b9)

--- yidun/service/antispam/media/v2/common/response/media_antispam_response.go
+++ yidun/service/antispam/media/v2/common/response/media_antispam_response.go
@@ -7,11 +7,13 @@ type MediaAntispamResponse struct {
 	// 机审结果
 	DataId                 *string                           `json:"dataId,omitempty"`
 	TaskId                 *string                           `json:"taskId,omitempty"`
-	Suggestion             *int                              `json:"suggestion,omitempty"`  // 0:无结果（检测失败）1:正常 2:异常 3：疑似
-	Label                  *int                              `json:"label,omitempty"`       // 垃圾类别
-	SecondLabel            *string                           `json:"secondLabel,omitempty"` // 二级机审标签
-	ThirdLabel             *string                           `json:"thirdLabel,omitempty"`  // 三级机审标签
-	ResultType             *int                              `json:"resultType,omitempty"`  // 审核类型，1:机器检测，2:审核
+	Suggestion             *int                              `json:"suggestion,omitempty"`      // 0:无结果（检测失败）1:正常 2:异常 3：疑似
+	SuggestionLevel        *int                              `json:"suggestionLevel,omitempty"` // 嫌疑级别，针对部分定制策略支持命中嫌疑时返回，1：低嫌疑，2：高嫌疑
+	RiskDescription        *string                           `json:"riskDescription,omitempty"` // 风险描述，拼接 label｜secondlabel｜thirdlabel 的中文实时示意
+	Label                  *int                              `json:"label,omitempty"`           // 垃圾类别
+	SecondLabel            *string                           `json:"secondLabel,omitempty"`     // 二级机审标签
+	ThirdLabel             *string                           `json:"thirdLabel,omitempty"`      // 三级机审标签
+	ResultType             *int                              `json:"resultType,omitempty"`      // 审核类型，1:机器检测，2:审核
 	Callback               *string                           `json:"callback,omitempty"`
 	CheckStatus            *int                              `json:"checkStatus,omitempty"`            // 检测状态, 1:检测中 2:检测成功 3:检测失败
 	Evidences              *envidence.MediaAntispamEvidence  `json:"evidences,omitempty"`              // 底层业务返回结果
