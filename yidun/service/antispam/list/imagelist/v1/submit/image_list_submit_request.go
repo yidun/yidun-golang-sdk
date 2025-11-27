cae2483db64f42f6c1cc5d848ac431dbfa26e5b8
niechengxuan01 <pb.niechengxuan@mesg.corp.netease.com>
2025-11-27 20:23:40 +0800

    OMSEC-138632 【后端】【图库添加接口】增加「嫌疑级别」「名单细分类」字段
    (cherry picked from commit 48685b2776e1ea69c7693d05c5015c2abd04e2d3)

--- yidun/service/antispam/list/imagelist/v1/submit/image_list_submit_request.go
+++ yidun/service/antispam/list/imagelist/v1/submit/image_list_submit_request.go
@@ -17,6 +17,8 @@ type ImageListSubmitRequest struct {
 	ListType *int `json:"listType,omitempty"`
 	// 名单分类（加黑名单时传），100：色情，110：性感，200：广告，210：二维码，300：暴恐，400：违禁，500：涉政
 	ImageLabel *int `json:"imageLabel,omitempty"`
+	// 名单二级分类
+	ImageSubLabel *string `json:"imageSubLabel,omitempty"`
 	// 名单类型，0: 相似名单，1: md5精确名单
 	Type *int `json:"type,omitempty"`
 	// 图片类型，包括url，base64编码等
@@ -29,6 +31,8 @@ type ImageListSubmitRequest struct {
 	TagName *string `json:"tagName,omitempty"`
 	// 业务id
 	BusinessId *string `json:"businessId,omitempty"`
+	// 嫌疑级别
+	SuspectLevel *int `json:"suspectLevel,omitempty"`
 }
 
 // 参数校验方法
@@ -79,6 +83,11 @@ func (req *ImageListSubmitRequest) SetImageLabel(imageLabel int) {
 	req.ImageLabel = &imageLabel
 }
 
+// SetImageSubLabel sets the ImageSubLabel field.
+func (req *ImageListSubmitRequest) SetImageSubLabel(imageSubLabel string) {
+	req.ImageSubLabel = &imageSubLabel
+}
+
 // SetType sets the Type field.
 func (req *ImageListSubmitRequest) SetType(typ int) {
 	req.Type = &typ
@@ -109,6 +118,11 @@ func (req *ImageListSubmitRequest) SetBusinessId(businessId string) {
 	req.BusinessId = &businessId
 }
 
+// SetSuspectLevel sets the SuspectLevel field.
+func (req *ImageListSubmitRequest) SetSuspectLevel(suspectLevel int) {
+	req.SuspectLevel = &suspectLevel
+}
+
 // GetCustomSignParams 获取具体业务中特有的需要参与签名计算的参数
 func (req *ImageListSubmitRequest) GetBusinessCustomSignParams() map[string]string {
 	params := req.PostFormRequest.GetBusinessCustomSignParams()
@@ -121,6 +135,9 @@ func (req *ImageListSubmitRequest) GetBusinessCustomSignParams() map[string]stri
 	}
 	if req.ImageLabel != nil {
 		params["imageLabel"] = strconv.Itoa(*req.ImageLabel)
+	}
+	if req.ImageSubLabel != nil {
+		params["imageSubLabel"] = *req.ImageSubLabel
 	}
 	if req.Type != nil {
 		params["type"] = strconv.Itoa(*req.Type)
@@ -139,6 +156,9 @@ func (req *ImageListSubmitRequest) GetBusinessCustomSignParams() map[string]stri
 	}
 	if req.BusinessId != nil {
 		params["businessId"] = *req.BusinessId
+	}
+	if req.SuspectLevel != nil {
+		params["suspectLevel"] = strconv.Itoa(*req.SuspectLevel)
 	}
 	return params
 }
