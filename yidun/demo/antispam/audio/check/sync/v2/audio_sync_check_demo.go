0d12446c7b3f0ea71a6a86042d5e7b13cb680a0f
yeqiang02 <yeqiang02@corp.netease.com>
2026-01-08 20:15:22 +0800

    OMSEC-152095 【后端】【点播音频】同步接口go sdk支持base64
    【后端】【点播音频】同步接口go sdk支持base64
    
    (cherry picked from commit 87c22a4ba17719feef3fc65e4ba970c5da7dce14)

--- yidun/demo/antispam/audio/check/sync/v2/audio_sync_check_demo.go
+++ yidun/demo/antispam/audio/check/sync/v2/audio_sync_check_demo.go
@@ -11,26 +11,20 @@ import (
 	"github.com/yidun/yidun-golang-sdk/yidun/core/http"
 )
 
-// 音频同步检测demo
-func main() {
-	// 设置易盾内容安全分配的businessId
-	YourBusinessId := os.Getenv("BUSINESS_ID")
-	YourSecretId := os.Getenv("SECRET_ID")
-	YourSecretKey := os.Getenv("SECRET_KEY")
-	request := request3.NewAudioSyncCheckRequest(YourBusinessId)
+// 执行音频同步检测
+func checkAudio(client *audio.AudioClient, businessId string, setupRequest func(*request3.AudioSyncCheckRequest)) {
+	request := request3.NewAudioSyncCheckRequest(businessId)
+	// 设置扩展字段
 	request.SetExtLon1(1)
 	request.SetExtLon2(2)
 	request.SetExtStr1("extStr1")
 	request.SetExtStr2("extStr2")
-
-	// 实例化一个Client，入参需要传入易盾内容安全分配的secretId，secretKey
-	client := audio.NewAudioClientWithAccessKey(YourSecretId, YourSecretKey)
-
-	// 设置 url
-	request.SetURL("http://xxx.mp4?time=1700213386514")
 	// 设置协议为HTTP
 	request.SetProtocol(http.ProtocolEnumHTTP)
 	request.SetUniqueKey(time.Now().String())
+
+	// 调用设置函数来配置特定的检测方式
+	setupRequest(request)
 
 	syncCheckResponse, err := client.SyncCheckAudio(request)
 	if err != nil {
@@ -45,4 +39,31 @@ func main() {
 		fmt.Println("error code: ", syncCheckResponse.GetCode())
 		fmt.Println("error msg: ", syncCheckResponse.GetMsg())
 	}
+}
+
+// 音频同步检测demo
+func main() {
+	// 设置易盾内容安全分配的businessId
+	YourBusinessId := os.Getenv("BUSINESS_ID")
+	YourSecretId := os.Getenv("SECRET_ID")
+	YourSecretKey := os.Getenv("SECRET_KEY")
+
+	// 实例化一个Client，入参需要传入易盾内容安全分配的secretId，secretKey
+	client := audio.NewAudioClientWithAccessKey(YourSecretId, YourSecretKey)
+
+	// URL方式检测示例
+	fmt.Println("=== URL方式检测 ===")
+	checkAudio(client, YourBusinessId, func(request *request3.AudioSyncCheckRequest) {
+		// 设置 url
+		request.SetURL("http://xxx.mp4?time=1700213386514")
+	})
+
+	// BASE64方式检测示例
+	fmt.Println("\n=== BASE64方式检测 ===")
+	checkAudio(client, YourBusinessId, func(request *request3.AudioSyncCheckRequest) {
+		// 设置检测类型为BASE64
+		request.SetDataCheckType(int(request3.DataCheckTypeBASE64))
+		// 设置base64编码的音频内容
+		request.SetData("base64编码的音频内容...")
+	})
 }
--- yidun/service/antispam/audio/check/sync/v2/request/audio_sync_check_reqeuest.go
+++ yidun/service/antispam/audio/check/sync/v2/request/audio_sync_check_reqeuest.go
@@ -1,11 +1,27 @@
 package request
 
 import (
+	"strconv"
+
 	"github.com/yidun/yidun-golang-sdk/yidun/service/antispam/audio/check"
+)
+
+// DataCheckType 检测类型枚举
+type DataCheckType int
+
+const (
+	// DataCheckTypeURL URL类型
+	DataCheckTypeURL DataCheckType = 0
+	// DataCheckTypeBASE64 BASE64类型
+	DataCheckTypeBASE64 DataCheckType = 1
 )
 
 type AudioSyncCheckRequest struct {
 	*check.AudioCommonCheckRequest
+	// 检测类型
+	DataCheckType *int `json:"dataCheckType,omitempty"`
+	// 同步检测接口-语音内容base64
+	Data *string `json:"data,omitempty"`
 }
 
 // NewAudioSyncCheckRequest 初始化AudioSyncCheckRequest对象
@@ -22,5 +38,23 @@ func NewAudioSyncCheckRequest(businessId string) *AudioSyncCheckRequest {
 // GetCustomSignParams 获取具体业务中特有的需要参与签名计算的参数
 func (r *AudioSyncCheckRequest) GetBusinessCustomSignParams() map[string]string {
 	params := r.AudioCommonCheckRequest.GetBusinessCustomSignParams()
+	if r.DataCheckType != nil {
+		params["dataCheckType"] = strconv.Itoa(*r.DataCheckType)
+	}
+	if r.Data != nil {
+		params["data"] = *r.Data
+	}
 	return params
+}
+
+// SetDataCheckType 设置检测类型
+func (r *AudioSyncCheckRequest) SetDataCheckType(dataCheckType int) *AudioSyncCheckRequest {
+	r.DataCheckType = &dataCheckType
+	return r
+}
+
+// SetData 设置同步检测接口-语音内容base64
+func (r *AudioSyncCheckRequest) SetData(data string) *AudioSyncCheckRequest {
+	r.Data = &data
+	return r
 }
